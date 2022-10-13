9d8bf46455286144a1917ae5c10f3e1b94101e15
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-10-13 18:44:41 -0300

    finish project [?]

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -6,9 +6,10 @@ export interface TextProps {
     size?: 'sm' | 'md' | 'lg'
     children: ReactNode
     asChild?: boolean
+    className?: string
 }
 
-export const Text = ({ size = 'md', children, asChild }: TextProps) => {
+export const Text = ({ size = 'md', children, asChild, className }: TextProps) => {
     const Comp = asChild ? Slot : 'span'
 
     return (
@@ -19,7 +20,8 @@ export const Text = ({ size = 'md', children, asChild }: TextProps) => {
                     'text-xs': size === 'sm',
                     'text-sm': size === 'md',
                     'text-md': size === 'lg',
-                }
+                },
+                className,
             )}
         >
             {children}
